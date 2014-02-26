0276b9b6223fcd7561582ccf1f0fc77776864db7
fhconsulting <harringfed@hotmail.com>
2014-02-26 18:44:15 -0200

    Update follows.js

--- follows.js
+++ follows.js
@@ -15,7 +15,7 @@
 	var defaultKey		= 'O1gd3yykB3lO6zCEjjKsTiwTrrsWJMEKEhDd81yLBAVrzQQ0', // Unique master Xively API key to be used as a default
 		defaultFeeds	= [53026], // Comma separated array of Xively Feed ID numbers
 		applicationName	= 'Milestone2', // Replaces Xively logo in the header
-		dataDuration	= '90days', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
+		dataDuration	= '1day', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 10800, // Default interval for data to be displayed (in seconds)
 		dataColor		= '0A1922', // CSS HEX value of color to represent data (omit leading #)
 		hideForm		= 0; // To hide input form use value of 1, otherwise set to 0
